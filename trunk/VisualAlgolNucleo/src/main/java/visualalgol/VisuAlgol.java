bb063976a812318b80482d478cc9b221d9964632
clemilsonbar <clemilsonbar@cffa2754-fdea-11de-bcdd-59c7212cd493>
2010-05-03 23:30:33 +0000

    Arrumei o pseudo Codigo Java (Carlos Java)

--- trunk/VisualAlgolNucleo/src/main/java/visualalgol/VisuAlgol.java
+++ trunk/VisualAlgolNucleo/src/main/java/visualalgol/VisuAlgol.java
@@ -1,5 +1,8 @@
 package visualalgol;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import visualalgol.swing.MainFrame;
 
 /**
@@ -8,7 +11,26 @@ import visualalgol.swing.MainFrame;
  */
 public class VisuAlgol {
 
+	
+	
 	public static void main(String[] args) {
+		
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnsupportedLookAndFeelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
 		new MainFrame();
 	}
 
