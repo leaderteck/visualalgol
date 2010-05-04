91f11f10bcd2c339ad5c9696f74aef3343df8892
fabio.oshiro <fabio.oshiro@cffa2754-fdea-11de-bcdd-59c7212cd493>
2010-05-04 03:39:19 +0000

    Bugfix: Erro ao remover o foco

--- trunk/VisualAlgolNucleo/src/main/java/visualalgol/ferramenta/CondicaoFimFerramenta.java
+++ trunk/VisualAlgolNucleo/src/main/java/visualalgol/ferramenta/CondicaoFimFerramenta.java
@@ -9,6 +9,7 @@ public class CondicaoFimFerramenta extends Ferramenta{
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if (getInstrucaoEm(e.getX(), e.getY()) == null) {
+			removerTodosOsFocos();
 			CondicaoFim condicaoFim = new CondicaoFim();
 			condicaoFim.setX(e.getX());
 			condicaoFim.setY(e.getY());
@@ -18,8 +19,6 @@ public class CondicaoFimFerramenta extends Ferramenta{
 			getAlgoritmo().getListComando().add(condicaoFim);
 			condicaoFim.setAlgoritmo(getAlgoritmo());
 			setArrastando(condicaoFim);
-		}else{
-			removerTodosOsFocos();
 		}
 	}
 }
