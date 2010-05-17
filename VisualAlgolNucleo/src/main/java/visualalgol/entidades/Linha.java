cf28a78eacbb1782df7829da8e9ca9417c2c4950
fabio.oshiro <fabio.oshiro@cffa2754-fdea-11de-bcdd-59c7212cd493>
2010-05-17 04:55:28 +0000

    Linha hashCode

--- VisualAlgolNucleo/src/main/java/visualalgol/entidades/Linha.java
+++ VisualAlgolNucleo/src/main/java/visualalgol/entidades/Linha.java
@@ -80,24 +80,32 @@ public class Linha implements Serializable, Cloneable{
 		return executado;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
 			return false;
-		if (!(obj instanceof Linha))
+		if (getClass() != obj.getClass())
 			return false;
 		Linha other = (Linha) obj;
-		long myId = this.getId();
-		if(myId!=other.getId())
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
-
+	
 	private static long unique = 0;
 	public synchronized long getId(){
 		if(id==null){
@@ -122,6 +130,7 @@ public class Linha implements Serializable, Cloneable{
 		return "Linha id " + getId() + "; origem = " + origem + "; destino = " + destino;
 	}
 
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
